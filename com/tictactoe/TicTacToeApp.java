09e54385bf6e8d699f2dd1d483cf1e3bcc236b34
sanjana270307-cpu <sanjana270307@gmail.com>
2026-04-21 20:16:40 +0530

    Added UC5: Validate user move (bounds + empty cell check)

--- com/tictactoe/TicTacToeApp.java
+++ com/tictactoe/TicTacToeApp.java
@@ -2,16 +2,31 @@ package com.tictactoe;
 
 import com.tictactoe.uc3.UserInput;
 import com.tictactoe.uc4.SlotConverter;
+import com.tictactoe.uc5.MoveValidator;
 
 public class TicTacToeApp {
 
     public static void main(String[] args) {
 
+        // Create empty board
+        char[][] board = {
+                {'-', '-', '-'},
+                {'-', '-', '-'},
+                {'-', '-', '-'}
+        };
+
         int slot = UserInput.getUserSlot();
 
-        int[] position = SlotConverter.getRowCol(slot);
+        int[] pos = SlotConverter.getRowCol(slot);
+        int row = pos[0];
+        int col = pos[1];
 
-        System.out.println("Row: " + position[0]);
-        System.out.println("Column: " + position[1]);
+        boolean isValid = MoveValidator.isValidMove(board, row, col);
+
+        if (isValid) {
+            System.out.println("Move accepted at Row: " + row + ", Column: " + col);
+        } else {
+            System.out.println("Try again!");
+        }
     }
 }
